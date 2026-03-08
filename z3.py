c9ea9adc0b66dc027ede4662d0a0524f2fdada1e
matejpizza <maatej.pizzaa@gmail.com>
2026-03-08 10:01:16 +0100

    Add files via upload

---
+++ z3.py
@@ -0,0 +1,42 @@
+br = int(input("Unesi jedan cijeli broj :"))
+
+if br%2 == 0:
+    print("Broj je paran")
+else:
+    print("Broj je neparan")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Program pita korisnika jedan cijeli broj.
+# Ako je djeljiv s 2 → "Paran"
+# Inače → "Neparan"
